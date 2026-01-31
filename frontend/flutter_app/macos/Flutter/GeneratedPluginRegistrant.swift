daccbebcd07cdb0c39b126b9f973fe5c8cc8947b
akshayks13 <akshayks1005@gmail.com>
2026-01-31 15:04:31 +0530

    Added contenirazation

--- frontend/flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import file_selector_macos
 import flutter_secure_storage_macos
 import flutter_tts
 import geolocator_apple
-import package_info_plus
 import path_provider_foundation
 import rive_common
 import shared_preferences_foundation
@@ -23,7 +22,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
